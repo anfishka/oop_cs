8c0787734b41183a159c08b45d9c4ce2400b88ed
anfishka <97487005+anfishka@users.noreply.github.com>
2023-12-15 00:10:26 +0200

    add lock class

--- Product.cs
+++ Product.cs
@@ -30,9 +30,22 @@ class Program
     }
 
 
-    class Lock
+    public class Lock
     {
+        public string AcceptedKeyType { get; set; }
+        public string Brand { get; set; }
+        public string ManufacturingCountry { get; set; }
 
+        public Lock(string acceptedKeyType, string brand, string manufacturingCountry)
+        {
+            this.AcceptedKeyType = acceptedKeyType;
+            this.Brand = brand;
+            this.ManufacturingCountry = manufacturingCountry;
+        }
+        public bool IsKeyCompatible(Key key)
+        {
+            return key.LockType == AcceptedKeyType && key.Brand == Brand && key.ManufacturingСountry == ManufacturingCountry;
+        }
     }
 
 
@@ -40,6 +53,9 @@ class Program
     {
 
         Key demoKey = new Key("Cylinder Lock", "Standard", "ABC Company", "Ireland", 6.0f, 4.0f, 12, 100.5f, "Silver");
+        Lock demoLock = new Lock("Cylinder Lock", "ABC Company", "Ireland");
+
+        bool isCompatible = demoLock.IsKeyCompatible(demoKey);
 
         Console.WriteLine($"LockType: {demoKey.LockType}");
         Console.WriteLine($"Compatibility: {demoKey.Compatibility}");
@@ -49,7 +65,12 @@ class Program
         Console.WriteLine($"Waranty: {demoKey.Waranty} months");
         Console.WriteLine($"Price: {demoKey.Price} grn");
         Console.WriteLine($"Color: {demoKey.Color}");
+
+        Console.WriteLine("\n*************************************\n");
+        Console.WriteLine($"AcceptedKeyType: {demoLock.AcceptedKeyType}");
+        Console.WriteLine($"Brand: {demoLock.Brand}");
+        Console.WriteLine($"ManufacturingCountry: {demoLock.ManufacturingCountry}");
+
+        Console.WriteLine(isCompatible == true ? "\nKey is compatible with the lock" : "Key is not compatible with the lock");
     }
-
-
- }
+    }
